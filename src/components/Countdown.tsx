194b0f33b025f3935316702a75420813dab25d30
Alessandro Gentil <vyseclown@gmail.com>
2021-02-27 15:06:55 -0300

    feat: adding countdown and challenges

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,28 +1,19 @@
-import { useState, useEffect } from "react";
+import { useContext } from "react";
+import { CountdownContext } from "../contexts/CountdownContext";
 import styles from "../styles/components/Countdown.module.css";
+
 export function Countdown() {
+  const {
+    isActive,
+    hasFinished,
+    minutes,
+    seconds,
+    resetCountdown,
+    startCountdown,
+  } = useContext(CountdownContext);
 
-    const [time, setTime] = useState(25*60);
-    const [active, setActive] = useState(false)
-    
-    const minutes = Math.floor(time / 60);
-
-    const seconds = time % 60;
-
-    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
-    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
-
-    function startCountdown() {
-        setActive(true)
-    }
-
-    useEffect(() => {
-        if(active && time > 0){
-            setTimeout(() => {
-                setTime(time -1)
-            }, 1000)
-        }
-    },[active, time])
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
+  const [secondLeft, secondRight] = String(seconds).padStart(2, "0").split("");
 
   return (
     <div>
@@ -38,11 +29,31 @@ export function Countdown() {
         </div>
       </div>
 
-      <button type="button" className={styles.countdownButton}
-      onClick={startCountdown}
-      >
-        Iniciar um ciclo
-      </button>
+      {hasFinished ? (
+        <button disabled type="button" className={styles.countdownButton}>
+          Ciclo encerrado
+        </button>
+      ) : (
+        <>
+          {isActive ? (
+            <button
+              type="button"
+              className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+              onClick={resetCountdown}
+            >
+              Abandonar ciclo
+            </button>
+          ) : (
+            <button
+              type="button"
+              className={styles.countdownButton}
+              onClick={startCountdown}
+            >
+              Iniciar um ciclo
+            </button>
+          )}
+        </>
+      )}
     </div>
   );
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,8 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/Profile.module.css";
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/vyseclown.png" alt="Alessandro Gentil" />
@@ -7,7 +10,7 @@ export function Profile() {
         <strong>Alessandro Gentil</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
